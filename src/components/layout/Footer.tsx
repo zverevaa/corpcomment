aa68b2abbbbfcaad550b4926ca1308d501497333
Kira Zvereva <angryvex@gmail.com>
2024-10-19 19:09:28 +0300

    Type error message

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
-
-
 export default function Footer() {
-  return (
-	<footer className='footer'><small><p>&copy; Copyright by Bytegrad. Intended for learning</p></small></footer>
-  )
+    return (
+        <footer className="footer">
+            <small>
+                <p>&copy; Copyright by Bytegrad. Intended for learning</p>
+            </small>
+        </footer>
+    );
 }
